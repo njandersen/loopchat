861a929b79fe963be39ac7f2bdd23c2ac1be99a7
njandersen <njandersen22@gmail.com>
2023-08-31 19:36:17 -0400

    add tweet functionality and structure for likes

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,16 +1,68 @@
-import { Button } from "./Button";
+import { useState, useLayoutEffect, useRef, useCallback } from "react";
+import type { FormEvent } from "react";
+import { useSession } from "next-auth/react";
 
-export function NewTweetForm() {
+import { Button } from "./Button";
+import ProfileImage from "./ProfileImage";
+import { api } from "~/utils/api";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
+
+function Form() {
+  const [input, setInput] = useState("");
+  const session = useSession();
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [input]);
+
+  if (session.status !== "authenticated") return null;
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      setInput("");
+    },
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    createTweet.mutate({ content: input });
+  };
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2 ">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2 "
+    >
       <div className="flex gap-4">
-        {/* <ProfileImage src="#url" /> */}
+        <ProfileImage src={session.data.user.image} />
         <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="Whats your loop?"
         />
       </div>
-      <Button>Tweet</Button>
+      <Button className="self-end">Tweet</Button>
     </form>
   );
+}
+
+export function NewTweetForm() {
+  const session = useSession();
+
+  if (session.status !== "authenticated") return;
+
+  return <Form />;
 }
