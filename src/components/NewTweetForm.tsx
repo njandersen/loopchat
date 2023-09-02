ecd5d18f217ad52e8d76aea70e75f4c008a33cf4
njandersen <njandersen22@gmail.com>
2023-09-02 12:34:50 -0400

    add data hydration to tweet creation

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -20,6 +20,7 @@ function Form() {
     updateTextAreaSize(textArea);
     textAreaRef.current = textArea;
   }, []);
+  const trpcUtils = api.useContext();
 
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
@@ -30,6 +31,31 @@ function Form() {
   const createTweet = api.tweet.create.useMutation({
     onSuccess: (newTweet) => {
       setInput("");
+      if (session.status !== "authenticated") return;
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData?.pages[0] == null) return;
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name ?? null,
+            image: session.data.user.image ?? null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
     },
   });
 
